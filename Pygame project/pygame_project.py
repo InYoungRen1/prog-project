e1583acf8b582e6602185fd9e3e469e1e1e67ee6
Jack Su <suj052003@gmail.com>
2021-01-28 11:41:08 -0800

    pygame projectv2.0

--- Pygame project/pygame_project.py
+++ Pygame project/pygame_project.py
@@ -11,6 +11,7 @@ SCREEN_HEIGHT = 600
 TITLE = "<Escape>"
 
 background_image = pygame.image.load("./image/background.png")
+background_image = pygame.transform.scale(background_image, (800, 600))
 
 # class
 class Player(pygame.sprite.Sprite):
@@ -22,11 +23,16 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         self.image = pygame.image.load("./image/oomba.png")
+        self.image = pygame.transform.scale(self.image, (85,85))
         self.rect = self.image.get_rect()
 
         # speed vector of player
         self.vel_x = 0
         self.vel_y = 0
+
+        self.jump_count = 0
+
+        self.level = None
 
     def update(self):
         """move the player"""
@@ -36,25 +42,8 @@ class Player(pygame.sprite.Sprite):
         # left and right
         self.rect.x += self.vel_x
 
-        # hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        for block in block_hit_list:
-            if self.vel_x > 0:
-                self.rect.right = block.rect_left
-            elif self.vel_x < 0:
-                self.rect.left = block.rect.right
-
         # up and down
         self.rect.y += self.vel_y
-
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        for block in block_hit_list:
-            if self.vel_y > 0:
-                self.rect.right = block.rect_top
-            elif self.vel_y < 0:
-                self.rect.left = block.rect.bottom
-            # stop vertical movement
-            self.vel_y = 0
 
     def calc_grav(self):
         """effect of gravity"""
@@ -66,24 +55,58 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.vel_y >= 0:
             self.vel_y = 0
             self.rect.y = SCREEN_HEIGHT - self.rect.height
+            self.jump_count = 0
 
     def jump(self):
         """jump"""
-        self.rect.y += 2
-        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        self.rect.y -= -2
-        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+        # set jump to 1
+        # if jump count is 1, then don't jump
+        if self.jump_count != 1:
+            self.jump_count += 1
             self.vel_y = -10
 
     # player controlled movement
     def go_left(self):
-        self.vel_y = -6
+        self.vel_x = -6
 
     def go_right(self):
         self.vel_x = 6
 
     def stop(self):
         self.vel_x = 0
+
+class Mario(pygame.sprite.Sprite):
+    """Enemy what walks at the bottom"""
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./image/mario.png.jpg")
+        self.image = pygame.transform.scale(self.image, (85, 85))
+        self.rect = self.image.get_rect()
+        self.x_vel = 3
+
+    def update(self):
+        self.rect.x += self.x_vel
+        if self.rect.right > SCREEN_WIDTH or self.rect.left < 0:
+            self.x_vel *= -1
+
+class Gold_coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./image/gold_coin.png")
+        self.image = pygame.transform.scale(self.image, (21, 36))
+        self.rect = self.image.get_rect()
+
+      # random location
+        self.rect.x = random.randrange(0, SCREEN_WIDTH)
+        self.rect.y = random.randrange(50, SCREEN_HEIGHT)
+        self.y_vel = random.randrange(1, 3)
+
+    def update(self):
+        """move gold coin down"""
+        self.rect.y += self.y_vel
+
+
 
 def main():
     pygame.init()
@@ -96,9 +119,18 @@ def main():
     # ----- LOCAL VARIABLES
     done = False
     clock = pygame.time.Clock()
+
+   # sprite groups
+    all_sprite = pygame.sprite.Group()
+    mario_sprite = pygame.sprite.Group()
+    gold_coin_sprites = pygame.sprite.Group()
+    # popular sprite Groups
+    mario = Mario()
+    mario.rect.y = SCREEN_HEIGHT - mario.rect.height
+    all_sprite.add(mario)
     player = Player()
-    all_sprite_list = pygame.sprite.Group()
-    all_sprite_list.add(player)
+    all_sprite.add(player)
+
 
     # ----- MAIN LOOP
     while not done:
@@ -121,15 +153,29 @@ def main():
                 if event.key == pygame.K_RIGHT and player.vel_x > 0:
                     player.stop()
 
-        # ----- LOGIC
+            if len(gold_coin_sprites) <= 6:
+                gold_coin = Gold_coin()
+                all_sprite.add(gold_coin)
+                gold_coin_sprites.add(gold_coin)
 
+        # ----- LOGIC
+        all_sprite.update()
         # ----- DRAW
         screen.fill(WHITE)
         screen.blit(background_image, [0,0])
+        all_sprite.draw(screen)
         # ----- UPDATE
         pygame.display.flip()
         clock.tick(60)
 
+        # remove gold coin
+        for gold_coin in gold_coin_sprites:
+            if gold_coin.rect.y < 0:
+                gold_coin.kill()
+        # collision
+        mario_hit_group = pygame.sprite.spritecollide(player, mario_sprite, True)
+        if len(mario_hit_group) > 0:
+            player.kill()
     pygame.quit()
 
 
